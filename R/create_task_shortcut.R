a4e81acc275db83a804bebb57c96117a276122db
DIAKITE Abdoul Oudouss <Diakson1441@gmail.com>
2023-05-18 17:46:00 +0100

    updated

--- R/create_task_shortcut.R
+++ R/create_task_shortcut.R
@@ -12,7 +12,7 @@ create_task_shortcut <- function(task_name, shortcut_name,appDir){
   # Get Os type
   sysname <- Sys.info()["sysname"]
   if (sysname == "Windows") {
-    # Créer le contenu du fichier .command
+    # Créer le contenu du fichier .bat
     contenu <- c("@echo off ",
                  paste0(Sys.which("Rscript.exe")," \"",
                         appDir,"/",task_name,".R\""),
@@ -22,7 +22,7 @@ create_task_shortcut <- function(task_name, shortcut_name,appDir){
   }
   else if (sysname == "Darwin") {
     # Créer le contenu du fichier .command
-    contenu <- paste0("#!/bin/bash\nRscript ", paste0(appDir,"/",task_name,".R"))
+    contenu <- paste0("#!/bin/bash\nRscript ", paste0("\"",appDir,"/",task_name,".R","\""))
 
     # Enregistrer le fichier .command
     writeLines(contenu, paste0(task_name, ".command"))
@@ -35,7 +35,7 @@ create_task_shortcut <- function(task_name, shortcut_name,appDir){
   }
   else if (sysname == "Linux") {
     # shell file contents
-    contenu <- paste0("#!/bin/bash\nRscript ", paste0(appDir,"/",task_name,".R"))
+    contenu <- paste0("#!/bin/bash\nRscript ", paste0("\"",appDir,"/",task_name,".R","\""))
     writeLines(contenu, paste0(task_name, ".sh"))
 
     # Make it executable
